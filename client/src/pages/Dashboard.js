e63d2326024fb383b3d6a70481a8c5d4e1edcb8a
Joe Loera <joeloera7@gmail.com>
2023-07-15 11:45:19 -0700

    Currently functioning app minus db

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,15 +1,43 @@
+import { Link } from 'react-router-dom'
+import Card from "../components/Card"
+
 const Dashboard = () => {
-
-    <div className="dashboard">
-        <div className="dashboard-info-container">
-            <div>
-                <h1>Adventure anywhere</h1>
-                <p>Keeyp calm & travel on</p>
+    const posts = [
+        {
+            id: 0,
+            title: "Lofoten Islands",
+            address: {
+                line: "House 45",
+                town: "Flakstad",
+                region: "Lofoten",
+                Country: "Norway",
+                coords: [13.615140, 68.148995]
+            },
+            photo: "https://www.fjordtravel.no/wp-content/uploads/2019/01/Amazing-Lofoten-Islands-by-Shutterstock-Hurtigruten.jpg",
+            website: "https://www.fjordtravel.no",
+            description: "Amazing Lofoten Islands",
+            tags: ["nature", "hiking", "mountains"]
+        },
+    ]
+return(
+    <div className="app">
+        <div className="dashboard">
+            <div className="dashboard-info-container">
+                <div>
+                    <h1>Adventure anywhere</h1>
+                    <p>Keep calm & travel on</p>
+                </div>
+                <button>Add your adventure</button>
             </div>
-            <button>Add your adventure</button>
+            <div className="posts-container">
+                {posts?.map ((post) => (
+                <Link to={`/posts/${post.id}`} id="link" key={post.id}>
+                    <Card  post={post}/>
+                </Link>
+                ))}
+            </div>
         </div>
-
-        <div className="posts-container"></div>
     </div>
+    )
 }
 export default Dashboard
