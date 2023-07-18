ae07d6fa3c70b7dddc0835eb9ac27663699c0b47
Joe Loera <joeloera7@gmail.com>
2023-07-17 21:29:57 -0700

    installing axios to client side

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,43 +1,31 @@
-import { Link } from 'react-router-dom'
-import Card from "../components/Card"
+import { Link } from "react-router-dom";
+import Card from "../components/Card";
+import axios from "axios";
 
 const Dashboard = () => {
-    const posts = [
-        {
-            id: 0,
-            title: "Lofoten Islands",
-            address: {
-                line: "House 45",
-                town: "Flakstad",
-                region: "Lofoten",
-                Country: "Norway",
-                coords: [13.615140, 68.148995]
-            },
-            photo: "https://www.fjordtravel.no/wp-content/uploads/2019/01/Amazing-Lofoten-Islands-by-Shutterstock-Hurtigruten.jpg",
-            website: "https://www.fjordtravel.no",
-            description: "Amazing Lofoten Islands",
-            tags: ["nature", "hiking", "mountains"]
-        },
-    ]
-return(
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8000/posts");
+    console.log(response);
+  };
+
+  return (
     <div className="app">
-        <div className="dashboard">
-            <div className="dashboard-info-container">
-                <div>
-                    <h1>Adventure anywhere</h1>
-                    <p>Keep calm & travel on</p>
-                </div>
-                <button>Add your adventure</button>
-            </div>
-            <div className="posts-container">
-                {posts?.map ((post) => (
-                <Link to={`/posts/${post.id}`} id="link" key={post.id}>
-                    <Card  post={post}/>
-                </Link>
-                ))}
-            </div>
+      <div className="dashboard">
+        <div className="dashboard-info-container">
+          <div>
+            <p>Keep calm & travel on</p>
+          </div>
+          <button>Add your adventure</button>
         </div>
+        <div className="posts-container">
+          {posts?.map((post) => (
+            <Link to={`/posts/${post.id}`} id="link" key={post.id}>
+              <Card post={post} />
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
-    )
-}
-export default Dashboard
+  );
+};
+export default Dashboard;
